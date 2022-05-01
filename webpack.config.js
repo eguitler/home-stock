6f4420e8bec97c6e5c82b49f84d00057f4b6ac6f
Ezequiel Guitler <eguitler@gmail.com>
2022-04-30 23:59:13 -0300

    [config] first project config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const rulesForJs = {
+  test: /\.js$/,
+  loader: 'babel-loader',
+  options: {
+    "presets": [
+      [
+        "@babel/preset-react",
+        {runtime: "automatic"}
+      ]
+    ]
+  }
+}
+
+const rulesForCss = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader']
+}
+
+module.exports = (env, argv) => {
+  const { mode } = argv
+  const isProduction = mode === 'production'
+
+  return {
+    output: {
+      filename: isProduction
+        ? '[name].[contentHash].js'
+        : 'main.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'public/index.html' })
+    ],
+    module: {
+      rules: [
+        rulesForJs,
+        rulesForCss,
+      ]
+    },
+    devServer: {
+      port: 3000,
+      client: {
+        overlay: true,
+      }
+    },
+    // devtool: 'source-map'
+  }
+}
